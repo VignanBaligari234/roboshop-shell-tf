649b40089a76c653b358bc3df3113ba343ab6c94
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-12 17:33:00 +0530

    added cart script and cart.repo

---
+++ cart.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+USERID=$(id -u)
+DATE=$(date +%F:%H:%M:%S)
+LOG_FILE_DIRECTORY=/tmp
+SCRIPT_NAME=$0
+LOGFILE=$LOG_FILE_DIRECTORY/$0-$DATE.log
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "installing nodejs"
+
+useradd roboshop &>>$LOGFILE
+
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>$LOGFILE
+
+VALIDATE $? "downloading cart artifact"
+
+cd /app &>>$LOGFILE 
+
+VALIDATE $? "moving to app directory"
+
+unzip /tmp/cart.zip &>>$LOGFILE 
+
+VALIDATE $? "unzipping the cart.zip"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "moving to app directory"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "installing dependencies"
+
+cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service &>>$LOGFILE
+
+VALIDATE $? "copying cart.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable cart &>>$LOGFILE
+
+VALIDATE $? "enabling cart"
+
+systemctl start cart &>>$LOGFILE
+
+VALIDATE $? "starting the cart"
+

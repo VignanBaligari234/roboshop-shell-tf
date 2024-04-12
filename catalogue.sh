0817a2cd8dfbf35cad24c83dd6f026cab7bd2491
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-12 11:11:25 +0530

    added catalogue script

---
+++ catalogue.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+USERID=$(id -u)
+DATE=$(date +%F:%H:%M:%S)
+LOG_FILE_DIRECTORY=/tmp/
+SCRIPT_NAME=$0
+LOGFILE=$LOG_FILE_DIRECTORY/$0-$DATE.log
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "installing nodejs"
+
+useradd roboshop &>>$LOGFILE
+
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+
+VALIDATE $? "downloading catalogue artifact"
+
+cd /app &>>$LOGFILE 
+
+VALIDATE $? "moving to app directory"
+
+unzip /tmp/catalogue.zip &>>$LOGFILE 
+
+VALIDATE $? "unzipping the catalogue.zip"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "installing dependencies"
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+
+VALIDATE $? "copying catalogie.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable catalogue &>>$LOGFILE
+
+VALIDATE $? "enabling catalogue"
+
+systemctl start catalogue &>>$LOGFILE
+
+VALIDATE $? "starting the catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "copying catalogue.repo"
+
+yum install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "installing mongodb client"
+
+mongo --host mangodb.vignanspace.online </app/schema/catalogue.js &>>$LOGFILE
+
+VALIDATE $? "loading the catalogue data into mongodb" 

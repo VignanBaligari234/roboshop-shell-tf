6c7a87344112d83aaca34b68539991b39b2a332a
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-04-13 12:46:49 +0530

    created ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-NAMES=("mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
-IMAGE_ID=ami-0a699202e5027c10d
-SECURITY_GROUP_ID=sg-0aa81d8005abce19b
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-0189d4870cc65fd55
+DOMAIN_NAME=vignanspace.online
+
+# if mysql or mongodb instance_type should be t3.medium , for all others it is t2.micro
 
 for i in "${NAMES[@]}"
-do
+do  
     if [[ $i == "mongodb" || $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
-    else 
+    else
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.instances[0].PrivateIpAddress')
-    echo "created $i instance:$IP_ADDRESS"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z02363281OB5K9FSAJUWC --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
 done
+
+# imporvement
+# check instance is already created or not
+# update route53 record

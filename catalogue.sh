4146fc847f8a7261ee5ab37f4b74a4a136286705
VignanBaligari234 <114463516+VignanBaligari234@users.noreply.github.com>
2024-09-25 02:03:36 +0530

    user data

--- catalogue.sh
+++ catalogue.sh
@@ -58,7 +58,7 @@ npm install &>>$LOGFILE
 
 VALIDATE $? "installing dependencies"
 
-cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+cp /root/roboshop-shell-tf/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.service"
 
@@ -74,7 +74,7 @@ systemctl start catalogue &>>$LOGFILE
 
 VALIDATE $? "starting the catalogue"
 
-cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+cp /root/roboshop-shell-tf/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
 
 VALIDATE $? "copying catalogue.repo"
 
